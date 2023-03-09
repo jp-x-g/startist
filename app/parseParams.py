1f6b72aa18e50a6f14e2795b4174b6a835a01373
jp-x-g <jpxg-dev@protonmail.com>
2023-03-09 13:46:32 -0800

    attempt to fix issue with date parsing (don't know why this got messed up in the first place!)

--- app/parseParams.py
+++ app/parseParams.py
@@ -1,7 +1,6 @@
 docstring = """
  JPxG, 2022 February 12
- This program does one of two things.
- The first thing
+ This program does things.
 """
 
 from datetime import date
@@ -22,72 +21,72 @@ def mapping(key="checkbox"):
 	mappingDict = {}
 
 	mappingDict["namespaces"] = {
-		"01":  [0,    "",                                       "MAIN"],
-		"01t": [1,    "Talk:",                                  "T"],
-		"02":  [2,    "User:",                                  "U"],
-		"02t": [3,    "User_talk:",                             "UT"],
-		"03":  [4,    "Wikipedia:",                             "WP"],
-		"03t": [5,    "Wikipedia_talk:",                        "WT"],
-		"04":  [6,    "File:",                                  "F"],
-		"04t": [7,    "File_talk:",                             "FT"],
-		"05":  [8,    "MediaWiki:",                             "MW"],
-		"05t": [9,    "MediaWiki_talk:",                        "MWT"],
-		"06":  [10,   "Template:",                              "T"],
-		"06t": [11,   "Template_talk:",                         "TT"],
-		"07":  [12,   "Help:",                                  "H"],
-		"07t": [13,   "Help_talk:",                             "HT"],
-		"08":  [14,   "Category:",                              "C"],
-		"08t": [15,   "Category_talk:",                         "CT"],
-		"09":  [100,  "Portal:",                                "P"],
-		"09t": [101,  "Portal_talk:",                           "PT"],
-		"10":  [118,  "Draft:",                                 "D"],
-		"10t": [119,  "Draft_talk:",                            "DT"],
-		"11":  [710,  "TimedText:",                             "TT"],
-		"11t": [711,  "TimedText_talk:",                        "TTT"],
-		"12":  [828,  "Module:",                                "M"],
-		"12t": [829,  "Module_talk:",                           "MT"],
-		"13":  [2300, "Gadget:",                                "G"],
-		"13t": [2301, "Gadget_talk:",                           "GT"],
-		"14":  [2302, "Gadget_definition:",                     "GD"],
-		"14t": [2303, "Gadget_definition_talk:",                "GDT"]
+		"01":    [0,    "",                                          "MAIN"],
+		"01t":   [1,    "Talk:",                                     "T"],
+		"02":    [2,    "User:",                                     "U"],
+		"02t":   [3,    "User_talk:",                                "UT"],
+		"03":    [4,    "Wikipedia:",                                "WP"],
+		"03t":   [5,    "Wikipedia_talk:",                           "WT"],
+		"04":    [6,    "File:",                                     "F"],
+		"04t":   [7,    "File_talk:",                                "FT"],
+		"05":    [8,    "MediaWiki:",                                "MW"],
+		"05t":   [9,    "MediaWiki_talk:",                           "MWT"],
+		"06":    [10,   "Template:",                                 "T"],
+		"06t":   [11,   "Template_talk:",                            "TT"],
+		"07":    [12,   "Help:",                                     "H"],
+		"07t":   [13,   "Help_talk:",                                "HT"],
+		"08":    [14,   "Category:",                                 "C"],
+		"08t":   [15,   "Category_talk:",                            "CT"],
+		"09":    [100,  "Portal:",                                   "P"],
+		"09t":   [101,  "Portal_talk:",                              "PT"],
+		"10":    [118,  "Draft:",                                    "D"],
+		"10t":   [119,  "Draft_talk:",                               "DT"],
+		"11":    [710,  "TimedText:",                                "TT"],
+		"11t":   [711,  "TimedText_talk:",                           "TTT"],
+		"12":    [828,  "Module:",                                   "M"],
+		"12t":   [829,  "Module_talk:",                              "MT"],
+		"13":    [2300, "Gadget:",                                   "G"],
+		"13t":   [2301, "Gadget_talk:",                              "GT"],
+		"14":    [2302, "Gadget_definition:",                        "GD"],
+		"14t":   [2303, "Gadget_definition_talk:",                   "GDT"]
   	}
 	mappingDict["noticeboards"] = {
-		"01":  [4, "Administrators'_noticeboard",               "AN"],
-		"02":  [4, "Administrators'_noticeboard/AN3",           "AN3"],
-		"03":  [4, "Administrators'_noticeboard/Incidents",     "ANI"],
-		"04":  [4, "Arbitration/Requests/Enforcement",          "AE"],
-		"05":  [4, "Biographies_of_living_persons/Noticeboard", "BLPN"],
-		"06":  [4, "Bots/Noticeboard",                          "BOTN"],
-		"07":  [4, "Bureaucrats'_noticeboard",                  "BN"],
-		"08":  [4, "Conflict_of_interest/Noticeboard",          "COIN"],
-		"09":  [4, "Dispute_resolution_noticeboard",            "DRN"],
-		"10":  [4, "External_links/Noticeboard",                "ELN"],
-		"11":  [4, "Fringe_theories/Noticeboard",               "FTN"],
-		"12":  [4, "No_original_research/Noticeboard",          "NORN"],
-		"13":  [4, "Neutral_point_of_view/Noticeboard",         "NPOVN"],
-		"14":  [4, "Reliable_sources/Noticeboard",              "RSN"]
+		"01":    [4,    "Administrators'_noticeboard",               "AN"],
+		"02":    [4,    "Administrators'_noticeboard/AN3",           "AN3"],
+		"03":    [4,    "Administrators'_noticeboard/Incidents",     "ANI"],
+		"04":    [4,    "Arbitration/Requests/Enforcement",          "AE"],
+		"05":    [4,    "Biographies_of_living_persons/Noticeboard", "BLPN"],
+		"06":    [4,    "Bots/Noticeboard",                          "BOTN"],
+		"07":    [4,    "Bureaucrats'_noticeboard",                  "BN"],
+		"08":    [4,    "Conflict_of_interest/Noticeboard",          "COIN"],
+		"09":    [4,    "Dispute_resolution_noticeboard",            "DRN"],
+		"10":    [4,    "External_links/Noticeboard",                "ELN"],
+		"11":    [4,    "Fringe_theories/Noticeboard",               "FTN"],
+		"12":    [4,    "No_original_research/Noticeboard",          "NORN"],
+		"13":    [4,    "Neutral_point_of_view/Noticeboard",         "NPOVN"],
+		"14":    [4,    "Reliable_sources/Noticeboard",              "RSN"]
 	}
 	mappingDict["refdesks"] = {
-		"01": [4, "Reference_desk",                             "RD"],
-		"02": [4, "Reference_desk/Computing",                   "RD/COMP"],
-		"03": [4, "Reference_desk/Entertainment",               "RD/ENT"],
-		"04": [4, "Reference_desk/Humanities",                  "RD/HUM"],
-		"05": [4, "Reference_desk/Language",                    "RD/LANG"],
-		"06": [4, "Reference_desk/Mathematics",                 "RD/MATH"],
-		"07": [4, "Reference_desk/Science",                     "RD/SCI"],
-		"08": [4, "Reference_desk/Miscellaneous",               "RD/MISC"]
+		"01":    [4,    "Reference_desk",                            "RD"],
+		"02":    [4,    "Reference_desk/Computing",                  "RD/COMP"],
+		"03":    [4,    "Reference_desk/Entertainment",              "RD/ENT"],
+		"04":    [4,    "Reference_desk/Humanities",                 "RD/HUM"],
+		"05":    [4,    "Reference_desk/Language",                   "RD/LANG"],
+		"06":    [4,    "Reference_desk/Mathematics",                "RD/MATH"],
+		"07":    [4,    "Reference_desk/Science",                    "RD/SCI"],
+		"08":    [4,    "Reference_desk/Miscellaneous",              "RD/MISC"]
 	}
 	mappingDict["villagepumps"] = {
-		"01": [4, "Village_pump_(policy)",                      "VP/P"],
-		"02": [4, "Village_pump_(technical)",                   "VP/T"],
-		"03": [4, "Village_pump_(proposals)",                   "VP/PR"],
-		"04": [4, "Village_pump_(idea lab)",                    "VP/IL"],
-		"05": [4, "Village_pump_(WMF)",                         "VP/WMF"],
-		"06": [4, "Village_pump_(miscellaneous)",               "VP/M"]
+		"01":    [4,    "Village_pump_(policy)",                     "VP/P"],
+		"02":    [4,    "Village_pump_(technical)",                  "VP/T"],
+		"03":    [4,    "Village_pump_(proposals)",                  "VP/PR"],
+		"04":    [4,    "Village_pump_(idea lab)",                   "VP/IL"],
+		"05":    [4,    "Village_pump_(WMF)",                        "VP/WMF"],
+		"06":    [4,    "Village_pump_(miscellaneous)",              "VP/M"]
 	}
 	mappingDict["misc"] = {
-		"helpdesk":     [4, "Help_desk",                        "HD"],
-		"teahouse":     [4, "Teahouse",                         "TH"]
+		"helpdesk": [4, "Help_desk",                                 "HD"],
+		"teahouse": [4, "Teahouse",                                  "TH"]
 	}
 
 	if (key == "checkbox"):
@@ -105,34 +104,34 @@ def mapping(key="checkbox"):
 
 	if (key == "namespaces"):
 		namespaceDict = {
-		0:    ["",                        "MAIN"],
-		1:    ["Talk:",                   "T"],
-		2:    ["User:",                   "U"],
-		3:    ["User_talk:",              "UT"],
-		4:    ["Wikipedia:",              "WP"],
-		5:    ["Wikipedia_talk:",         "WT"],
-		6:    ["File:",                   "F"],
-		7:    ["File_talk:",              "FT"],
-		8:    ["MediaWiki:",              "MW"],
-		9:    ["MediaWiki_talk:",         "MWT"],
-		10:   ["Template:",               "T"],
-		11:   ["Template_talk:",          "TT"],
-		12:   ["Help:",                   "H"],
-		13:   ["Help_talk:",              "HT"],
-		14:   ["Category:",               "C"],
-		15:   ["Category_talk:",          "CT"],
-		100:  ["Portal:",                 "P"],
-		101:  ["Portal_talk:",            "PT"],
-		118:  ["Draft:",                  "D"],
-		119:  ["Draft_talk:",             "DT"],
-		710:  ["TimedText:",              "TT"],
-		711:  ["TimedText_talk:",         "TTT"],
-		828:  ["Module:",                 "M"],
-		829:  ["Module_talk:",            "MT"],
-		2300: ["Gadget:",                 "G"],
-		2301: ["Gadget_talk:",            "GT"],
-		2302: ["Gadget_definition:",      "GD"],
-		2303: ["Gadget_definition_talk:", "GDT"]
+		0:    ["",                                                   "MAIN"],
+		1:    ["Talk:",                                              "T"],
+		2:    ["User:",                                              "U"],
+		3:    ["User_talk:",                                         "UT"],
+		4:    ["Wikipedia:",                                         "WP"],
+		5:    ["Wikipedia_talk:",                                    "WT"],
+		6:    ["File:",                                              "F"],
+		7:    ["File_talk:",                                         "FT"],
+		8:    ["MediaWiki:",                                         "MW"],
+		9:    ["MediaWiki_talk:",                                    "MWT"],
+		10:   ["Template:",                                          "T"],
+		11:   ["Template_talk:",                                     "TT"],
+		12:   ["Help:",                                              "H"],
+		13:   ["Help_talk:",                                         "HT"],
+		14:   ["Category:",                                          "C"],
+		15:   ["Category_talk:",                                     "CT"],
+		100:  ["Portal:",                                            "P"],
+		101:  ["Portal_talk:",                                       "PT"],
+		118:  ["Draft:",                                             "D"],
+		119:  ["Draft_talk:",                                        "DT"],
+		710:  ["TimedText:",                                         "TT"],
+		711:  ["TimedText_talk:",                                    "TTT"],
+		828:  ["Module:",                                            "M"],
+		829:  ["Module_talk:",                                       "MT"],
+		2300: ["Gadget:",                                            "G"],
+		2301: ["Gadget_talk:",                                       "GT"],
+		2302: ["Gadget_definition:",                                 "GD"],
+		2303: ["Gadget_definition_talk:",                            "GDT"]
 		}
 		return namespaceDict
 
@@ -265,7 +264,11 @@ def parse(params):
 		output["enddate"] = valiDate(params["enddate"])
 
 	today = datetime.today()
-	daysOffset = 30 * 6
+	## Six months.
+	# daysOffset = 30 * 6
+
+	# Six hundred years.
+	 daysOffset = 365 * 600
 	offset = timedelta(
 		days=daysOffset
 	)
@@ -281,7 +284,7 @@ def parse(params):
 	# Create a datetime object from the oldest date.
 
 	if oldestDate < backthen:
-		#output["startdate"] = backthenFormat
+		output["startdate"] = backthenFormat
 		# If oldest date is too far back, set it to the oldest allowed date.
 		pass
 
